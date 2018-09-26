e48c8fb460de8d948b7003ce9d0948b09927c794
Oleksandr Khomenko <khomenko.dp@gmail.com>
2018-09-26 22:04:34 +0300

    add import of local class

--- front-end/app.js
+++ front-end/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Datatable from './test';
 
 var api_url = URL;
 
@@ -7,7 +8,12 @@ ReactDOM.render(
   <div>
     <a href = {api_url}>Link</a>
     <h1>dsdsd</h1>
+    <h1>s</h1>
+    <ul>
+      <li>lol</li>
+    </ul>
     <h2>{api_url}</h2>
+    <Datatable/>
   </div>,
   document.getElementById('root')
 );
